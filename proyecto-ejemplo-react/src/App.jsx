9c79c51aaa562d8b5f58372c02f7971851f175a9
Jose Germán Martínez <jgermanmm@gmail.com>
2025-01-23 12:00:23 -0300

    Agregar enrutamiento con react-router-dom y componente NavBar

--- proyecto-ejemplo-react/src/App.jsx
+++ proyecto-ejemplo-react/src/App.jsx
@@ -1,13 +1,25 @@
 
 import './App.css'
 import ListaUsuarios from './components/ListaUsuarios'
+import Contador from './components/Contador'
+import Formulario from './components/Formulario'
+import {BrowserRouter, Routes, Route  } from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+
 
 function App() {
 
   return (
-    <>
-     <ListaUsuarios />
-    </>
+    <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<h1>Bienvenidos</h1>} />
+      <Route path="/contador" element={<Contador />} />
+      <Route path="/formulario" element={<Formulario />} />
+      <Route path="/usuarios" element={<ListaUsuarios />} />
+      <Route path="*" element={<h1>404</h1>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- proyecto-ejemplo-react/src/components/NavBar/NavBar.jsx
+++ proyecto-ejemplo-react/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom"
 
 
 function NavBar() {
   return (
     <nav>
         <ul>
-            <li>Inicio</li>
-            <li>Productos</li>
-            <li>Contacto</li>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/contador">Contador</Link></li>
+            <li><Link to="/formulario">Formulario</Link></li>
+            <li><Link to="/usuarios">Lista usuarios</Link></li>
         </ul>
     </nav>
   )
